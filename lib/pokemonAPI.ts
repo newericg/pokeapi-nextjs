87f0aa91b16a66f26c42c8553213dd70fc6b2b92
newericg1~ <eric.gomes0105@gmail.com>
2024-04-19 12:42:47 -0300

    poke project first version

---
+++ lib/pokemonAPI.ts
@@ -0,0 +1,33 @@
+
+const POKEMON_API = "https://pokeapi.co/api/v2/";
+
+// getPokemonList -> Get the first 151 pokemon 
+export async function getPokemonList() {
+    const response = await fetch(POKEMON_API + "pokemon?limit=151&offset=0");
+    const data = await response.json();
+    return data.results;
+}
+
+// getPokemon -> given a string "pikachu", get the information of pikachu
+export async function getPokemon(name: string) {
+    const response = await fetch(POKEMON_API + "pokemon/" + name);
+    const data = await response.json();
+    return data;
+}
+
+
+// getPokemonText -> given a id, get pokemon flavor text
+export async function getPokemonText(name: string) {
+    const response = await fetch(POKEMON_API + "pokemon-species/" + name);
+    const data = await response.json();
+    for (let entry of data.flavor_text_entries) {
+        if (entry.language.name === "en") {
+          let flavor = entry.flavor_text.replace(/\f/g, " ");
+          return flavor;
+        }
+      }
+      return "";
+
+
+    // return console.log(data.flavor_text_entries)
+}
--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "raw.githubusercontent.com",
+            },
+            {
+                protocol: "https",
+                hostname: "pokeapi.co",
+                pathname: 'api/v2/pokemon/**',
+            },
+        ]
+    }
+}
 
 module.exports = nextConfig
