a42db746a46825e08a61b984e43de41a002d49df
newericg1~ <eric.gomes0105@gmail.com>
2024-04-22 15:21:27 -0300

    feat: effects on pokemon cards page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import { PokemonGrid } from "@components";
-
-console.log(PokemonGrid);
 import { getPokemonList } from "@/lib/pokemonAPI";
 
 export default async function Home({ searchParams }: { searchParams: any }) {
   const pokemonList = await getPokemonList({
     page: searchParams?.page ?? 1,
-    limit: 20,
+    limit: 151,
   });
 
   return <PokemonGrid pokemonList={pokemonList} />;
--- components/pokemon-card.tsx
+++ components/pokemon-card.tsx
@@ -10,21 +10,26 @@ const PokemonCard = ({ name, pokeId }: PokemonCardProps) => {
   return (
     <CustomLink
       href={name}
-      className="group rounded-lg border border-gray m-3 px-5 py-4 transition-colors dark:border-gray-500 hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 shadow-inner"
+      className="card flex flex-col items-center justify-center group rounded-lg border border-gray m-3 px-5 py-2 transition-colors dark:border-gray-500 hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 shadow-inner"
     >
-      <Image
-        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeId}.png`}
-        alt={"Picture of " + name}
-        priority
-        width={50}
-        height={50}
-        style={{ objectFit: "contain" }}
-        className="transition-opacity opacity-0 duration-[2s]"
-        onLoadingComplete={(image) => image.classList.remove("opacity-0")}
-      /> 
-      <h2 className={`text-2xl font-semibold`}>
-        {name.charAt(0).toUpperCase() + name.slice(1)}
-      </h2>
+      <div className="flex justify-between w-full items-center card-title">
+        <p className={`ml-2`}> {pokeId.padStart(4, "#00")} </p>
+        <h2 className={`text-2xl font-semibold mr-2`}>
+          {name.charAt(0).toUpperCase() + name.slice(1)}
+        </h2>
+      </div>
+      
+        <Image
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeId}.png`}
+          alt={"Picture of " + name}
+          priority
+          width={150}
+          height={150}
+          style={{ objectFit: "contain" }}
+          className="transition-opacity opacity-0 duration-[2s] image-filter"
+          onLoadingComplete={(image) => image.classList.remove("opacity-0")}
+        />
+      
     </CustomLink>
   );
 };
